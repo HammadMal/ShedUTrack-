e68bafc848a12d6dcdc7ab77cfc104f20f67a4d0
HammadMal <122615617+HammadMal@users.noreply.github.com>
2024-12-03 18:19:08 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,5 +37,9 @@ app.get('/', (req, res) => {
     res.send('Server is running');
 });
 
+app.get('/api/auth', (req, res) => {
+    res.send('auth is running');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
